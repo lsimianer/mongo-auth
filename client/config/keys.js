7486a86d7512158ff0d551899d613f54c4dd4058
lukas simianer <lsimianer@gmail.com>
2019-08-02 10:31:59 -0500

    auth stuff push to new repo for sharing

---
+++ client/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb+srv://lsimianer:+simianer1+@cluster0-7z1it.mongodb.net/test?retryWrites=true&w=majority" ,
+    secretOrKey: "secret"
+  };
+
+  
---
+++ client/server.js
@@ -0,0 +1,58 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const passport = require("passport");
+
+// const users = require("../Routes/api/users");
+// const app = express();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('../Routes/api/users');
+
+const app = express();
+app.use(express.static('public'));
+
+
+//middleware
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next();
+});
+
+// Bodyparser middleware
+
+app.use(
+    bodyParser.urlencoded({extended: false})
+);
+app.use(
+    bodyParser.json()
+);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(()=> console.log("MongoDB successfully connected."))
+    .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Routes
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server up and runnint on port ${port} !`));
